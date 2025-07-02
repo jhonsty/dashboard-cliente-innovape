0a977a984fdbd4eb0881f295f279e9fe21d1b191
Jhonstiber Ramos <ingbim.ichaccaya@gmail.com>
2025-07-02 02:56:36 -0500

    Corrijo Procfile y actualizo app.py

--- Prueba_2/app.py
+++ Prueba_2/app.py
@@ -70,5 +70,8 @@ def dashboard():
         monto_por_banco=monto_por_banco
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
